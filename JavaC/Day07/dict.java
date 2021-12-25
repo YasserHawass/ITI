fe6e8fc55865ee48fdb327166873d8d33356101b
Yasser Hawass <yasser.hawass@gmail.com>
2021-12-25 10:33:25 +0200

    Adding Documentation

--- JavaC/Day07/dict.java
+++ JavaC/Day07/dict.java
@@ -7,6 +7,20 @@ import java.util.Collections;
 import java.util.List;
 import java.util.*;
 
+/*
+Word Dictionary
+- Develop and application that stores words in an array into a
+collection.
+- Create a map that uses the alphabets as keys and a
+collection as values (This collection should only contain
+words starting with the corresponding key)
+- Elements in the words map for each alphabet should be
+sorted
+- Provide methods to print all the letters and corresponding
+words
+- Provide a method to print the words of a given letter
+*/
+
 class dict{
 // yes it's best practice to be Capital, but for OS directory easiness
 	public static void main(String args[]){
