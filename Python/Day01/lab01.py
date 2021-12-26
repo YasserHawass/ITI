c2a471fc6cd68d0c388f7de200c77ddc71c1b100
Yasser Hawass <yasser.hawass@gmail.com>
2021-12-26 14:02:49 +0200

    Add Python first lab

---
+++ Python/Day01/lab01.py
@@ -0,0 +1,34 @@
+users = {'ali':'123', 'omar':'456', 'ahmed':'789'}
+name = input("What is your name? ").strip().lower()
+if (name == 'ali' or name == 'ahmed' or name == 'omar'):
+    password = input("What is your password? ")
+    if(users.get(name) == password):
+        print(f"Howdy's {name} !")
+    else:
+        print("invalid password")
+else:
+    print("invalid name")
+
+
+
+# also could be done
+# if name in users and password == users[name]:
+#     print("Logged in")
+# else:
+#     print("bad username / password")
+
+
+# or just go with multiple nested if's for each specific user to check if their password is correct
+# maybe also do a switch case
+
+
+
+
+
+# there's also a bonus assignment
+# how to count 'ehm' in a string if it's not in a word
+# sentence = "ehm I am a sentence notehm with ehm in it ehm"
+# s_count = sentence.startswith('ehm ')if 1 else 0
+# e_count = sentence.endswith(' ehm')if 1 else 0
+# count = sentence.count(' ehm ')
+# print( count + s_count + e_count)
---
+++ Python/Day01/test.py
@@ -0,0 +1,8 @@
+# x = 'hello'
+# print(f'{x[5::-1]}')
+# y = 'Hello' + ' ' + 'World'
+# print(y.strip())
+# print(y.find('o'))
+# print(y.count('o'))
+# print(y.replace('o'))
+
